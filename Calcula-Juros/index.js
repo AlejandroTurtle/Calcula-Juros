89fc4d09e2e59d86b5047094496f25fd916c2902
AlejandroTurtle <155404372+AlejandroTurtle@users.noreply.github.com>
2024-01-09 08:56:23 -0300

    Update index.js

--- Calcula-Juros/index.js
+++ Calcula-Juros/index.js
@@ -1,40 +1,40 @@
-import entradaDados from 'readline-sync';
+import entradaDados from 'readline-sync'
 
-console.log("Aplicação de Juros: ");
+console.log("Aplicação de Juros: ")
 
-let valor_juros;
+let valor_juros
 
-let valor_divida = entradaDados.question("\nInforme o valor devido: R$ ");
+let valor_divida = entradaDados.question("\nInforme o valor devido: R$ ")
 
 if(valor_divida > 0)
 {
-    let dias_vencimento = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ");
+    let dias_vencimento = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ")
 
     if(dias_vencimento > 0)
     {
         if(dias_vencimento > 15)
         {
-            valor_juros = 10;
+            valor_juros = 10
         }
         else
         {
-            valor_juros = 5;
+            valor_juros = 5
         }
 
-        valor_divida = Number(valor_divida);
-        dias_vencimento = Number(dias_vencimento);
+        valor_divida = Number(valor_divida)
+        dias_vencimento = Number(dias_vencimento)
 
-        let total_juros = (valor_divida / 100) * valor_juros;
-        let valor_total = valor_divida + total_juros;
+        let total_juros = (valor_divida / 100) * valor_juros
+        let valor_total = valor_divida + total_juros
 
-        console.log("\nValor original da dívida: R$ "+valor_divida);
-        console.log("Dias atrasados: "+dias_vencimento);
-        console.log("Taxa de Juros: "+valor_juros+"%");
-        console.log("Valor total com juros: R$ "+valor_total);
+        console.log("\nValor original da dívida: R$ "+valor_divida)
+        console.log("Dias atrasados: "+dias_vencimento)
+        console.log("Taxa de Juros: "+valor_juros+"%")
+        console.log("Valor total com juros: R$ "+valor_total)
     }
     else
     {
-        console.log("Você está em dia!");
+        console.log("Você está em dia!")
     }
 }
 else
